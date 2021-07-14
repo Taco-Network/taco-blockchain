76206a3f5e9c6ebd49d905023e1be902fd80a2b1
SVGInsomnia <svginsomnia@gmail.com>
2021-07-14 02:56:38 -0400

    fix

--- taco/cmds/configure.py
+++ taco/cmds/configure.py
@@ -101,7 +101,7 @@ def configure(
         elif testnet == "false" or testnet == "f":
             print("Setting Mainnet")
             mainnet_port = "6888"
-            mainnet_introducer = "dns-introducer.inspecturgadget.net"
+            mainnet_introducer = "dns-introducer.taconetwork.net"
             net = "mainnet"
             config["full_node"]["port"] = int(mainnet_port)
             config["full_node"]["introducer_peer"]["port"] = int(mainnet_port)
