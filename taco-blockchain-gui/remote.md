2f91500b3ca487b875f9f5d471e2185ee319af77
SVGInsomnia <svginsomnia@gmail.com>
2021-07-16 22:55:26 -0400

    2.0

--- taco-blockchain-gui/remote.md
+++
@@ -1,3 +0,0 @@
-# Connecting the UI to a Remote Daemon
-
-_This instructional how-to has been updated and [moved to the wiki](https://github.com/Taco-Network/taco-blockchain/wiki/Connecting-the-UI-to-a-remote-daemon)._
--- taco-blockchain-gui/src/components/icons/Taco.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { SvgIcon, SvgIconProps } from '@material-ui/core';
-import { ReactComponent as TacoIcon } from './images/taco.svg';
-
-export default function Keys(props: SvgIconProps) {
-  return <SvgIcon component={TacoIcon} viewBox="0 0 150 58" {...props} />;
-}
