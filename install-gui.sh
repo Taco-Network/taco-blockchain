7848fd57a132db9ffcd5e12a5fa6195b9e0b075f
SVGInsomnia <svginsomnia@gmail.com>
2021-07-16 22:37:37 -0400

    2.0

--- install-gui.sh
+++ install-gui.sh
@@ -1,69 +1,100 @@
 #!/bin/bash
 set -e
-export NODE_OPTIONS="--max-old-space-size=3000"
-
-
-if [ -z "$VIRTUAL_ENV" ]; then
-  echo "This requires the chia python virtual environment."
-  echo "Execute '. ./activate' before running."
-	exit 1
-fi
-
-if [ "$(id -u)" = 0 ]; then
-  echo "The Taco Blockchain GUI can not be installed or run by the root user."
-	exit 1
-fi
-
-# Allows overriding the branch or commit to build in taco-blockchain-gui
-SUBMODULE_BRANCH=$1
-
 UBUNTU=false
-# Manage npm and other install requirements on an OS specific basis
+DEBIAN=false
 if [ "$(uname)" = "Linux" ]; then
 	#LINUX=1
 	if type apt-get; then
-		# Debian/Ubuntu
-		UBUNTU=true
-		sudo apt-get install -y npm nodejs libxss1
-	elif type yum &&  [ ! -f "/etc/redhat-release" ] && [ ! -f "/etc/centos-release" ] && [ ! -f /etc/rocky-release ]; then
-		# AMZN 2
-		echo "Installing on Amazon Linux 2."
-		curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
-		sudo yum install -y nodejs
-	elif type yum && [ ! -f /etc/rocky-release ] && [ -f /etc/redhat-release ] || [ -f /etc/centos-release ]; then
-		# CentOS or Redhat
-		echo "Installing on CentOS/Redhat."
-		curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
-		sudo yum install -y nodejs
-	elif type yum && [ -f /etc/rocky-release ]; then
-                # RockyLinux
-                echo "Installing on RockyLinux"
-                dnf module enable nodejs:12
-                sudo dnf install -y nodejs
-        fi
-
-elif [ "$(uname)" = "Darwin" ] && type brew && ! npm version >/dev/null 2>&1; then
-	# Install npm if not installed
-	brew install npm
-elif [ "$(uname)" = "OpenBSD" ]; then
-	pkg_add node
-elif [ "$(uname)" = "FreeBSD" ]; then
-	pkg install node
+		OS_ID=$(lsb_release -is)
+		if [ "$OS_ID" = "Debian" ]; then
+			DEBIAN=true
+		else
+			UBUNTU=true
+		fi
+	fi
 fi
 
-# Ubuntu before 20.04LTS has an ancient node.js
-echo ""
+# Check for non 64 bit ARM64/Raspberry Pi installs
+if [ "$(uname -m)" = "armv7l" ]; then
+  echo ""
+	echo "WARNING:"
+	echo "The Taco Blockchain requires a 64 bit OS and this is 32 bit armv7l"
+	echo "For more information, see"
+	echo "https://github.com/Taco-Network/taco-blockchain/wiki/Raspberry-Pi"
+	echo "Exiting."
+	exit 1
+fi
+# Get submodules
+git submodule update --init mozilla-ca
+
 UBUNTU_PRE_2004=false
 if $UBUNTU; then
-	UBUNTU_PRE_2004=$(python -c 'import subprocess; process = subprocess.run(["lsb_release", "-rs"], stdout=subprocess.PIPE); print(float(process.stdout) < float(20.04))')
+	LSB_RELEASE=$(lsb_release -rs)
+	# In case Ubuntu minimal does not come with bc
+	if [ "$(which bc |wc -l)" -eq 0 ]; then sudo apt install bc -y; fi
+	# Mint 20.04 repsonds with 20 here so 20 instead of 20.04
+	UBUNTU_PRE_2004=$(echo "$LSB_RELEASE<20" | bc)
+	UBUNTU_2100=$(echo "$LSB_RELEASE>=21" | bc)
 fi
 
-if [ "$UBUNTU_PRE_2004" = "True" ]; then
-	echo "Installing on Ubuntu older than 20.04 LTS: Ugrading node.js to stable."
-	UBUNTU_PRE_2004=true # Unfortunately Python returns True when shell expects true
-	sudo npm install -g n
-	sudo n stable
-	export PATH="$PATH"
+# Manage npm and other install requirements on an OS specific basis
+if [ "$(uname)" = "Linux" ]; then
+	#LINUX=1
+	if [ "$UBUNTU" = "true" ] && [ "$UBUNTU_PRE_2004" = "1" ]; then
+		# Ubuntu
+		echo "Installing on Ubuntu pre 20.04 LTS."
+		sudo apt-get update
+		sudo apt-get install -y python3.7-venv python3.7-distutils
+	elif [ "$UBUNTU" = "true" ] && [ "$UBUNTU_PRE_2004" = "0" ] && [ "$UBUNTU_2100" = "0" ]; then
+		echo "Installing on Ubuntu 20.04 LTS."
+		sudo apt-get update
+		sudo apt-get install -y python3.8-venv python3-distutils
+	elif [ "$UBUNTU" = "true" ] && [ "$UBUNTU_2100" = "1" ]; then
+		echo "Installing on Ubuntu 21.04 or newer."
+		sudo apt-get update
+		sudo apt-get install -y python3.9-venv python3-distutils
+	elif [ "$DEBIAN" = "true" ]; then
+		echo "Installing on Debian."
+		sudo apt-get update
+		sudo apt-get install -y python3-venv
+	elif type pacman && [ -f "/etc/arch-release" ]; then
+		# Arch Linux
+		echo "Installing on Arch Linux."
+		sudo pacman -S --needed python git
+	elif type yum && [ ! -f "/etc/redhat-release" ] && [ ! -f "/etc/centos-release" ] && [ ! -f "/etc/fedora-release" ]; then
+		# AMZN 2
+		echo "Installing on Amazon Linux 2."
+		sudo yum install -y python3 git
+	elif type yum && [ -f "/etc/redhat-release" ] || [ -f "/etc/centos-release" ] || [ -f "/etc/fedora-release" ]; then
+		# CentOS or Redhat or Fedora
+		echo "Installing on CentOS/Redhat/Fedora."
+	fi
+elif [ "$(uname)" = "Darwin" ] && ! type brew >/dev/null 2>&1; then
+	echo "Installation currently requires brew on MacOS - https://brew.sh/"
+elif [ "$(uname)" = "OpenBSD" ]; then
+	export MAKE=${MAKE:-gmake}
+	export BUILD_VDF_CLIENT=${BUILD_VDF_CLIENT:-N}
+elif [ "$(uname)" = "FreeBSD" ]; then
+	export MAKE=${MAKE:-gmake}
+	export BUILD_VDF_CLIENT=${BUILD_VDF_CLIENT:-N}
+fi
+
+find_python() {
+	set +e
+	unset BEST_VERSION
+	for V in 37 3.7 38 3.8 39 3.9 3; do
+		if which python$V >/dev/null; then
+			if [ x"$BEST_VERSION" = x ]; then
+				BEST_VERSION=$V
+			fi
+		fi
+	done
+	echo $BEST_VERSION
+	set -e
+}
+
+if [ x"$INSTALL_PYTHON_VERSION" = x ]; then
+	INSTALL_PYTHON_VERSION=$(find_python)
 fi
 
 if [ "$UBUNTU" = "true" ] && [ "$UBUNTU_PRE_2004" = "False" ]; then
