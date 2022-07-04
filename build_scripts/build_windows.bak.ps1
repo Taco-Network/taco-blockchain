26cf47773d7c6995560c3997bcc06fbacf6a24ea
BTCgreen-Network <svginsomnia@gmail.com>
2022-07-04 02:58:43 -0400

    Build scripts

--- build_scripts/build_windows.bak.ps1
+++ build_scripts/build_windows.bak.ps1
@@ -3,64 +3,15 @@
 $ErrorActionPreference = "Stop"
 
 mkdir build_scripts\win_build
-Set-Location -Path ".\build_scripts\win_build" -PassThru
 
 git status
-
-Write-Output "   ---"
-Write-Output "curl miniupnpc"
-Write-Output "   ---"
-Invoke-WebRequest -Uri "https://pypi.chia.net/simple/miniupnpc/miniupnpc-2.1-cp37-cp37m-win_amd64.whl" -OutFile "miniupnpc-2.1-cp37-cp37m-win_amd64.whl"
-Write-Output "Using win_amd64 python 3.7 wheel from https://github.com/miniupnp/miniupnp/pull/475 (2.2.0-RC1)"
-If ($LastExitCode -gt 0){
-    Throw "Failed to download miniupnpc!"
-}
-else
-{
-    Set-Location -Path - -PassThru
-    Write-Output "miniupnpc download successful."
-}
-
-Write-Output "   ---"
-Write-Output "Create venv - python3.7 or 3.8 is required in PATH"
-Write-Output "   ---"
-python -m venv venv
-. .\venv\Scripts\Activate.ps1
-python -m pip install --upgrade pip
-pip install wheel pep517
-pip install pywin32
-pip install pyinstaller==4.2
-pip install setuptools_scm
-
-Write-Output "   ---"
-Write-Output "Get TACO_INSTALLER_VERSION"
-# The environment variable TACO_INSTALLER_VERSION needs to be defined
-$env:TACO_INSTALLER_VERSION = python .\build_scripts\installer-version.py -win
 
 if (-not (Test-Path env:TACO_INSTALLER_VERSION)) {
   $env:TACO_INSTALLER_VERSION = '0.0.0'
   Write-Output "WARNING: No environment variable TACO_INSTALLER_VERSION set. Using 0.0.0"
-  }
+}
 Write-Output "Taco Version is: $env:TACO_INSTALLER_VERSION"
 Write-Output "   ---"
-
-Write-Output "   ---"
-Write-Output "Build taco-blockchain wheels"
-Write-Output "   ---"
-pip wheel --use-pep517 --extra-index-url https://pypi.chia.net/simple/ -f . --wheel-dir=.\build_scripts\win_build .
-
-Write-Output "   ---"
-Write-Output "Install taco-blockchain wheels into venv with pip"
-Write-Output "   ---"
-
-Write-Output "pip install miniupnpc"
-Set-Location -Path ".\build_scripts" -PassThru
-pip install --no-index --find-links=.\win_build\ miniupnpc
-# Write-Output "pip install setproctitle"
-# pip install setproctitle==1.2.2
-
-Write-Output "pip install taco-blockchain"
-pip install --no-index --find-links=.\win_build\ taco-blockchain
 
 Write-Output "   ---"
 Write-Output "Use pyinstaller to create taco .exe's"
@@ -71,18 +22,33 @@ pyinstaller --log-level INFO $SPEC_FILE
 Write-Output "   ---"
 Write-Output "Copy taco executables to taco-blockchain-gui\"
 Write-Output "   ---"
-Copy-Item "dist\daemon" -Destination "..\taco-blockchain-gui\" -Recurse
+Copy-Item "dist\daemon" -Destination "..\taco-blockchain-gui\packages\gui\" -Recurse
+
+Write-Output "   ---"
+Write-Output "Setup npm packager"
+Write-Output "   ---"
+Set-Location -Path ".\npm_windows" -PassThru
+npm ci
+$Env:Path = $(npm bin) + ";" + $Env:Path
+Set-Location -Path "..\" -PassThru
+
 Set-Location -Path "..\taco-blockchain-gui" -PassThru
+# We need the code sign cert in the gui subdirectory so we can actually sign the UI package
+If ($env:HAS_SECRET) {
+    Copy-Item "win_code_sign_cert.p12" -Destination "packages\gui\"
+}
 
 git status
 
 Write-Output "   ---"
 Write-Output "Prepare Electron packager"
 Write-Output "   ---"
-npm install --save-dev electron-winstaller
-npm install -g electron-packager
-npm install
-npm audit fix
+$Env:NODE_OPTIONS = "--max-old-space-size=3000"
+
+lerna clean -y
+npm ci
+# Audit fix does not currently work with Lerna. See https://github.com/lerna/lerna/issues/1663
+# npm audit fix
 
 git status
 
@@ -94,6 +60,9 @@ If ($LastExitCode -gt 0){
     Throw "npm run build failed!"
 }
 
+# Change to the GUI directory
+Set-Location -Path "packages\gui" -PassThru
+
 Write-Output "   ---"
 Write-Output "Increase the stack for taco command for (taco plots create) chiapos limitations"
 # editbin.exe needs to be in the path
@@ -104,6 +73,15 @@ $packageVersion = "$env:TACO_INSTALLER_VERSION"
 $packageName = "Taco-$packageVersion"
 
 Write-Output "packageName is $packageName"
+
+Write-Output "   ---"
+Write-Output "fix version in package.json"
+choco install jq
+cp package.json package.json.orig
+jq --arg VER "$env:TACO_INSTALLER_VERSION" '.version=$VER' package.json > temp.json
+rm package.json
+mv temp.json package.json
+Write-Output "   ---"
 
 Write-Output "   ---"
 Write-Output "electron-packager"
@@ -128,6 +106,12 @@ If ($env:HAS_SECRET) {
 }
 
 git status
+
+Write-Output "   ---"
+Write-Output "Moving final installers to expected location"
+Write-Output "   ---"
+Copy-Item ".\Taco-win32-x64" -Destination "$env:GITHUB_WORKSPACE\taco-blockchain-gui\" -Recurse
+Copy-Item ".\release-builds" -Destination "$env:GITHUB_WORKSPACE\taco-blockchain-gui\" -Recurse
 
 Write-Output "   ---"
 Write-Output "Windows Installer complete"
